dfef979c5ec50fb4f9ea1be4757945bbf30be692
Evan <evanpalsson@gmail.com>
2016-08-30 10:07:48 -0600

    about page added, routing working as well

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,10 @@
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
 
 const APP_ROUTES: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
